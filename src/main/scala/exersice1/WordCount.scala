0dc3c4b3ca6163357cb014e653f2651134195d8d
s_labrinos <slabrinos9214@gmail.com>
2021-05-09 09:42:26 +0300

    exercise 1 completed

--- src/main/scala/exersice1/WordCount.scala
+++ src/main/scala/exersice1/WordCount.scala
@@ -1,23 +1,51 @@
 package exersice1
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext
 
 object WordCount {
   def main(args: Array[String]): Unit = {
-    println("hello world")
+    // Create a SparkContext using the local machine
+    val sc = new SparkContext("local[*]", "WordCount")
+    // Load each line of the book into an RDD
+    val input = sc.textFile("data/SherlockHolmes.txt")
+    // Split using a regular expression that extracts words into lines with flatMap
+    val lowercaseWords = input
+      .flatMap(_.split("\\W+"))
+      // Normalize everything to lowercase
+      .map(_.toLowerCase())
 
-    val ss = SparkSession
-      .builder()
-      .master("local")
-      .appName("WordCount")
-      .getOrCreate()
+    // Step 1
+    // Calculating total word count and length of every word
+    // Mapping each word/line in a tuple (word_length, 1)
+    val wordsLengthAndCount = lowercaseWords
+      .map(x => (x.length, 1))
+      .reduce((x, y) => (x._1 + y._1, x._2 + y._2))
+    // Getting the average word length of the file
+    val wordAvg: Float = wordsLengthAndCount._1 / wordsLengthAndCount._2.toFloat
 
-    val sc = ss.sparkContext
+    // Step 2
+    // Filter the words with length less than the wordAvg
+    val wordCounts = lowercaseWords
+      .filter(_.length.toFloat >= wordAvg)
+      // Count of the occurrences of each word
+      .map((_, 1))
+      .reduceByKey(_ + _)
+      // Flip (word, count) tuples to (count, word) and then sort by key (the counts)
+      .map(x => (x._2, x._1))
+      .sortByKey(ascending = false)
+      // Collecting the RDD results from the cluster (different cores for local execution)
+      .collect()
 
-    val data = Array(1, 2, 3, 4, 5)
-    val distData = sc.parallelize(data)
+    // Printing the results of the exercise
+    println("Total words found: " + wordsLengthAndCount._2)
+    val formatAverage = f"$wordAvg%.2f"
+    println(s"Average length of all the words: $formatAverage")
 
-    println(distData.sum())
+    // Print the results the first 5 results (count, word).
+    for (result <- wordCounts.take(5)) {
+      val count = result._1
+      val word = result._2
+      println(s"$word: $count")
+    }
   }
-
 }
